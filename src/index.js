25c9a7565d2e6d79234edf3a2e21f70c49b6c4b3
rodrigohyper <vergeofsound@gmail.com>
2021-10-14 12:07:47 -0300

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const inputElement = document.querySelector('input');
+const addButton = document.querySelector('form button');
+const ulElement = document.querySelector('ul');
+
+addButton.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    if(inputElement.value) {
+        const listElement = document.createElement('li');
+        const removeBtn = document.createElement('button');
+
+        listElement.innerHTML = `<span>${inputElement.value}</span>`;
+        removeBtn.innerHTML = `<i class="fas fa-trash-alt fa-sm"></i>`;
+
+        removeBtn.classList.add('remove-btn');
+
+        ulElement.appendChild(listElement);
+        listElement.appendChild(removeBtn);
+
+        inputElement.value = '';
+        
+        removeBtn.addEventListener('click', () => {
+            listElement.remove();
+        })
+
+        listElement.addEventListener('click', () => {
+            listElement.classList.toggle('checked-list');
+        })
+    }
+});
+
